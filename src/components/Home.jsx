31a5ae2a79ca2e35730235d40c30c27ef70f831b
Atul <atulsourabh083@gmail.com>
2024-01-05 21:04:16 +0530

    Added connect icons

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,18 +1,23 @@
 import React from 'react'
+import { TbBrandLinkedin, TbBrandGithub, TbBrandTwitter } from "react-icons/tb";
 
 const Home = () => {
   return (
-    <div className='bg-gray-100 '>
+    <div className='bg-gray-100 md:px-[4rem] pt-20 pb-32 lg:pt-24'>
 
-      <div className='flex flex-col-reverse gap-24 lg:flex-row items-center lg:gap-40 '>
+      <div className='flex flex-col-reverse gap-24 lg:flex-row lg:justify-between items-center '>
         
         <div className='max-w-[550px] text-center lg:text-left'>
             <div className='text-[65px] font-bold  leading-tight  '>Front-End React Developer 👋🏼</div>
             <p className="mt-4 text-xl font-medium">Hi, I'm Atul Sourav. A passionate Front-end React Developer based in Kolkata, India.</p>
-            <div></div>
+            <div className='mt-8'>
+                <TbBrandLinkedin  style={{display: 'inline', marginRight:'20px', fontSize:'40px'}} />
+                <TbBrandTwitter style={{display: 'inline', marginRight:'20px', fontSize:'38px'}} />
+                <TbBrandGithub style={{display: 'inline', marginRight:'20px', fontSize:'38px'}}/>
+            </div>
         </div>
         <div>
-            <div className="blob bg-profile w-[21rem] h-[21rem] bg-cover bg-center border-4 border-black shadow-[0px_5px_10px_5px_#a0aec0] "></div>
+            <div className="blob bg-profile min-w-[280px] h-[340px] bg-cover bg-center border-4 border-black shadow-[0px_5px_10px_5px_#a0aec0] "></div>
         </div>
         
       </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,7 @@ module.exports = {
   theme: {
     extend: {
       backgroundImage : {
-        'profile': "url('https://images.unsplash.com/photo-1513152697235-fe74c283646a?q=80&w=1248&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D')"
+        'profile': "url('./assets/profile.jpg')"
       }
     },
     // screens: {
